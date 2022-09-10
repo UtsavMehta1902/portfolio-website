4bda2af91cdd4ff75d469d3e98c69a209e3eccb9
Utsav Mehta <utsavmehta2002@yahoo.com>
2022-09-10 13:06:25 +0530

    Changed node version.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Container, Row, Col } from "react-bootstrap";
-
+import React from 'react';
 import navIcon1 from "../assets/img/nav-icon1.svg";
 import navIcon2 from "../assets/img/nav-icon2.svg";
 import githubIcon from "../assets/img/github5.svg";
